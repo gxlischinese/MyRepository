49bd1fa8bdc86bbc49ffd8b5f8ec22ecb38462f3
lenovo <lenovo@DESKTOP-9E983M6>
2018-06-23 23:42:48 +0800

    第二次

--- test/src/test/Test1.java
+++ test/src/test/Test1.java
@@ -64,13 +64,12 @@ public class Test1 {
         }
     }
 
-//   public static void main(String[] args) {
-       public static void main(String[] args) {
-           // TODO Auto-generated method stub
-           for (String arg : args) {
-               System.out.println("运行时的参数： " + arg);
-           }
-           System.out.println("kkd");
+   public static void main(String[] args) {
+       for (String arg : args) {
+           System.out.println("运行时的参数： " + arg);
        }
-//}
+       System.out.println("kkd");
+       System.out.println("kkd");
+       System.out.println("kkd");
+   }
 }
