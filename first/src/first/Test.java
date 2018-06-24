7aef805d8fcc81505840527b41b7bf8180f81918
gxlischinese <40412681+gxlischinese@users.noreply.github.com>
2018-06-24 10:09:47 +0800

    Update Test.java

--- first/src/first/Test.java
+++ first/src/first/Test.java
@@ -3,5 +3,6 @@ package first;
 public class Test {
     public static void main(String[] args) {
         System.out.println("this is first project saved on GitHub");
+        System.out.println("update on GitHub at 10:09");
     }
 }
